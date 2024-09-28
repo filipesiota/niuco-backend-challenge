b69e957b2628479aad947b21d2a542efbe889ef8
Filipe Mallmann Siota <filipe.siota@diarioescola.com.br>
2024-09-28 15:09:56 -0300

    FEAT: Add E2E and Unit Tests to solve the challenge (TDD)

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HttpService } from '@nestjs/axios';
+import { of } from 'rxjs';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,15 +10,78 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService,
+        {
+          provide: HttpService,
+          useValue: {
+            get: jest.fn().mockImplementation(() => 
+              of({
+                data: [
+                  {
+                    "id": "0373e634-2d03-457e-a24d-2b0c8c3b7c37",
+                    "name": "John Connor",
+                    "email": "john.connor@niuco.com.br",
+                    "status": "enabled",
+                    "role": "admin",
+                    "last_activity": 1649179152
+                  },
+                  {
+                    "id": "5fb75748-efa6-4d48-9930-14289d87466f",
+                    "name": "Kyle Reese",
+                    "email": "kyle.reese@gmail.com",
+                    "status": "disabled",
+                    "role": "editor",
+                    "last_activity": 1649073600
+                  },
+                  {
+                    "id": "4c3dfa4c-3cee-4acb-b032-c09afad54ab4",
+                    "name": "Bob Esponja",
+                    "email": "bob.esponja@niuco.com.br",
+                    "status": "enabled",
+                    "role": "viewer",
+                    "last_activity": 1649098800
+                  }
+                ]
+              })
+            )
+          }
+        }
+      ]
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('getUsers', () => {
+    it('should return a manipulated array of users based on an external API call', () => {
+      const extectedResponse = [
+        {
+          "id": "0373e634-2d03-457e-a24d-2b0c8c3b7c37",
+          "name": "John Connor",
+          "email": "john.connor@niuco.com.br",
+          "lastActivity": "2022-04-05T17:19:12",
+          "isPayer": true,
+          "isActive": true
+        },
+        {
+          "id": "5fb75748-efa6-4d48-9930-14289d87466f",
+          "name": "Kyle Reese",
+          "email": "ky******se@gmail.com",
+          "lastActivity": "2022-04-04T12:00:00",
+          "isPayer": false,
+          "isActive": false
+        },
+        {
+          "id": "4c3dfa4c-3cee-4acb-b032-c09afad54ab4",
+          "name": "Bob Esponja",
+          "email": "bob.esponja@niuco.com.br",
+          "lastActivity": "2022-04-04T19:00:00",
+          "isPayer": false,
+          "isActive": true
+        }
+      ];
+
+      expect(appController.getUsers()).toBe(extectedResponse);
     });
   });
 });
